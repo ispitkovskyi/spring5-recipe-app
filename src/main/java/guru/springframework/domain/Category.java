df5924d47a927389c642a0ab4fbb85035a53387c
ispitkovskyi <spitkovskyigor@gmail.com>
2022-09-19 16:38:29 +0300

    140. Spring Data JPA Query Methods

--- src/main/java/guru/springframework/domain/Category.java
+++ src/main/java/guru/springframework/domain/Category.java
@@ -16,11 +16,27 @@ public class Category {
     @ManyToMany(mappedBy = "categories") //"categories" - is name of variable in Recipe class, where mapping to Category is defined
     private Set<Recipe> recipes;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getDescription() {
         return description;
     }
 
     public void setDescription(String categoryName) {
         this.description = categoryName;
+    }
+
+    public Set<Recipe> getRecipes() {
+        return recipes;
+    }
+
+    public void setRecipes(Set<Recipe> recipes) {
+        this.recipes = recipes;
     }
 }
