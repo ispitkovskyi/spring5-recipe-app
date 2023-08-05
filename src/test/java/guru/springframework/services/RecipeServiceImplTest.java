c1c29f9969d899228874985dc8505251c56b1224
ispitkovskyi <spitkovskyigor@gmail.com>
2023-08-05 19:54:35 +0300

    using mockito mock of repository class

---
+++ src/test/java/guru/springframework/services/RecipeServiceImplTest.java
@@ -0,0 +1,51 @@
+package guru.springframework.services;
+
+import guru.springframework.domain.Recipe;
+import guru.springframework.repositories.RecipeRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+
+public class RecipeServiceImplTest {
+
+    RecipeServiceImpl recipeService;
+
+    //Will inject instance of recipeRepository, when MockitoAnnotations.initMocks(this) is called
+    @Mock
+    RecipeRepository recipeRepository;
+
+
+    @Before
+    public void setUp() throws Exception {
+        //Triggers injection of @Mock annotated instances
+        MockitoAnnotations.initMocks(this);
+        recipeService = new RecipeServiceImpl(recipeRepository);
+    }
+
+    @Test
+    public void getRecipes() {
+        Recipe recipe = new Recipe();
+        HashSet recipesData = new HashSet<>();
+        recipesData.add(recipe);
+
+        /** When recipeRepository.findAll() is called (it happens inside recipeService.getRecipes() method call)
+         * Then return the recipesData hashset
+         */
+
+        when(recipeRepository.findAll()).thenReturn(recipesData);
+
+        Set<Recipe> recipes = recipeService.getRecipes();
+        assertEquals(1, recipes.size());
+        /**Use Mockito to verify that recipeRepository instance will be used only 1 time
+        * (when recipeService.getRecipes() is called above)
+        */
+        verify(recipeRepository, times(1)).findAll();
+    }
+}
