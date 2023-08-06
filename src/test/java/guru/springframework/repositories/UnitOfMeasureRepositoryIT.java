75449f02f52b5b43df123d38cd838a2c4bb22d1b
ispitkovskyi <spitkovskyigor@gmail.com>
2023-08-06 15:51:26 +0300

    Spring Integration TEst

--- src/test/java/guru/springframework/repositories/UnitOfMeasureRepositoryIT.java
+++ src/test/java/guru/springframework/repositories/UnitOfMeasureRepositoryIT.java
@@ -6,7 +6,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Optional;
@@ -19,7 +18,7 @@ import static org.junit.Assert.assertEquals;
  */
 @RunWith(SpringRunner.class)
 @DataJpaTest
-public class UnitOfMeasureRepositoryIntegrationTest {
+public class UnitOfMeasureRepositoryIT {  //IT - default naming for Integration Tests for maven
 
     /** Spring will do dependency injection on the integration testing,
      *  so we’ll have an instance of UnitOfmeasureRepository class, injected into the test class.
@@ -32,7 +31,7 @@ public class UnitOfMeasureRepositoryIntegrationTest {
     }
 
     @Test
-    @DirtiesContext // - this annotations tells Spring that context should be reloaded. A bit slower, but sometimes you need that
+    //@DirtiesContext // - this annotations tells Spring that context should be reloaded. A bit slower, but sometimes you need that
     public void findByDescriptionTeaspoon() {
         Optional<UnitOfMeasure> unitOfMeasure = unitOfMeasureRepository.findByDescription("Teaspoon");
         assertEquals("Teaspoon", unitOfMeasure.get().getDescription());
