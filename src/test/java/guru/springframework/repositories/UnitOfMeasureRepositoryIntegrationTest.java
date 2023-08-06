13d4f35ad7812ddd9c80b9f1822e21a2db381c9d
ispitkovskyi <spitkovskyigor@gmail.com>
2023-08-06 15:43:11 +0300

    Spring Integration TEst

---
+++ src/test/java/guru/springframework/repositories/UnitOfMeasureRepositoryIntegrationTest.java
@@ -0,0 +1,47 @@
+package guru.springframework.repositories;
+
+import guru.springframework.domain.UnitOfMeasure;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Optional;
+
+import static org.junit.Assert.assertEquals;
+
+/** Integration test brings up the Spring context and embedded database for an application.
+ * @RunWith(SpringRunner.class) - runner for an integration test
+ * @DataJpaTest - it will bring up an embedded database and configure SpringData JPA for us
+ */
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class UnitOfMeasureRepositoryIntegrationTest {
+
+    /** Spring will do dependency injection on the integration testing,
+     *  so we’ll have an instance of UnitOfmeasureRepository class, injected into the test class.
+     */
+    @Autowired
+    UnitOfMeasureRepository unitOfMeasureRepository;
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    @DirtiesContext // - this annotations tells Spring that context should be reloaded. A bit slower, but sometimes you need that
+    public void findByDescriptionTeaspoon() {
+        Optional<UnitOfMeasure> unitOfMeasure = unitOfMeasureRepository.findByDescription("Teaspoon");
+        assertEquals("Teaspoon", unitOfMeasure.get().getDescription());
+    }
+
+    @Test
+    public void findByDescriptionCup() {
+        Optional<UnitOfMeasure> unitOfMeasure = unitOfMeasureRepository.findByDescription("Cup");
+        assertEquals("Cup", unitOfMeasure.get().getDescription());
+    }
+
+}
